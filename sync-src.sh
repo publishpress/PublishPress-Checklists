f133fa5fdd06a3c9a4284ae4ba83dd7fb4cde56f
Anderson Grudtner Martins <anderson@ostraining.com>
2017-04-14 16:37:09 -0300

    Add scripts to sync code

---
+++ sync-src.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Sync the src folder into wordpress, to auto-update the code while 
+# developing. Used with fswatch in Mac OS to detect file changes.
+
+rm -rf ./src/*
+cp -R ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-checklist/* ./src/
---
+++ sync-wp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Sync the src folder into wordpress, to auto-update the code while 
+# developing. Used with fswatch in Mac OS to detect file changes.
+
+rm -rf ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-checklist/*
+cp -R ./src/* ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-checklist/
