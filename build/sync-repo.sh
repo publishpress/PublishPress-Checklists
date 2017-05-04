34d67290c3f7863b8898ed1725ce91c8567e48f7
Anderson Grudtner Martins <anderson@ostraining.com>
2017-05-04 17:42:24 -0300

    Improves sync scripts

--- build/sync-repo.sh
+++ build/sync-repo.sh
@@ -3,5 +3,11 @@
 # Sync the src folder into wordpress, to auto-update the code while
 # developing. Used with fswatch in Mac OS to detect file changes.
 
-rm -rf ../plugin/*
-cp -R ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-content-checklist/* ../plugin/
+STAGING_PATH=~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-content-checklist
+SRC_PATH=../plugin
+
+
+if [ -d $STAGING_PATH ]; then
+	rm -rf $SRC_PATH/*
+	cp -R $STAGING_PATH/* $SRC_PATH/
+fi
--- build/sync-staging.sh
+++ build/sync-staging.sh
@@ -3,5 +3,13 @@
 # Sync the src folder into wordpress, to auto-update the code while
 # developing. Used with fswatch in Mac OS to detect file changes.
 
-rm -rf ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-content-checklist/*
-cp -R ../plugin/* ~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-content-checklist/
+STAGING_PATH=~/Projects/OSTraining/Git/dev-env-wordpress/www/wp-content/plugins/publishpress-content-checklist
+SRC_PATH=../plugin
+
+
+if [ ! -d $STAGING_PATH ]; then
+	mkdir $STAGING_PATH
+fi
+
+rm -rf $STAGING_PATH/*
+cp -R $SRC_PATH/* $STAGING_PATH
