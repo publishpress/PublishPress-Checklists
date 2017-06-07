a5e0e3bd2ce3a08bde4b9b69b807d4f573b4dd1b
Anderson Grudtner Martins <anderson@ostraining.com>
2017-06-07 17:03:19 -0300

    Update version

--- src/includes.php
+++ src/includes.php
@@ -89,7 +89,7 @@ if ( ! defined( 'PP_CONTENT_CHECKLIST_HALT' ) ) {
 		}
 
 		if ( ! defined( 'PP_CONTENT_CHECKLIST_VERSION' ) ) {
-			define( 'PP_CONTENT_CHECKLIST_VERSION', '1.2.0' );
+			define( 'PP_CONTENT_CHECKLIST_VERSION', '1.2.1a1' );
 		}
 
 		if ( ! defined( 'PP_CONTENT_CHECKLIST_MODULE_PATH' ) ) {
