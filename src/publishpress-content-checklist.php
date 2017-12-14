4848ed85fbfb24c72f194a7e2b8400a9ff3e8838
Anderson Grudtner Martins <anderson@ostraining.com>
2017-12-14 16:17:58 -0200

    Update version to 1.3.3b1

--- src/publishpress-content-checklist.php
+++ src/publishpress-content-checklist.php
@@ -12,7 +12,7 @@
  * @publishpress-content-checklist
  * Plugin Name: PublishPress Content Checklist
  * Plugin URI:  https://pressshack.com/publishpress/
- * Version:     1.3.3a1
+ * Version:     1.3.3b1
  * Description: Add a content checklist for posts
  * Author:      PressShack
  * Author URI:  https://pressshack.com
