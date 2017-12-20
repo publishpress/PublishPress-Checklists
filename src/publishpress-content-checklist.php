620a5e20ae71a33b23d299e1722ba370b1337802
Anderson Grudtner Martins <anderson@ostraining.com>
2017-12-20 18:45:32 -0200

    Bump beta version

--- src/publishpress-content-checklist.php
+++ src/publishpress-content-checklist.php
@@ -12,7 +12,7 @@
  * @publishpress-content-checklist
  * Plugin Name: PublishPress Content Checklist
  * Plugin URI:  https://pressshack.com/publishpress/
- * Version:     1.3.3b1
+ * Version:     1.3.3b2
  * Description: Add a content checklist for posts
  * Author:      PressShack
  * Author URI:  https://pressshack.com
