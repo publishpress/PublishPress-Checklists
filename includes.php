7e2c2f322cdeb5a5e1e4932815f6915a7c06dd3b
andergmartins <anderson@alledia.com>
2019-06-14 13:55:55 -0300

    Bump min required version of PublishPress to 1.20.0

--- includes.php
+++ includes.php
@@ -45,7 +45,7 @@ if ( ! defined(PP_HALT_CONSTANT) && ! defined('PP_CONTENT_CHECKLIST_LOADED')) {
     require_once $publishpressPath;
 
     if ( ! defined('PP_CONTENT_CHECKLIST_MIN_PARENT_VERSION')) {
-        define('PP_CONTENT_CHECKLIST_MIN_PARENT_VERSION', '1.6.0');
+        define('PP_CONTENT_CHECKLIST_MIN_PARENT_VERSION', '1.20.0');
     }
 
     /*==========================================================
