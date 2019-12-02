4a351c244e5d718d5bce980a606cc1a8d106d06c
Anderson Grudtner Martins <anderson@alledia.com>
2019-12-02 17:21:18 -0300

    Update version and remove some not used constants

--- includes.php
+++ includes.php
@@ -12,18 +12,14 @@
 defined('ABSPATH') or die('No direct script access allowed.');
 
 if ( ! defined('PUBLISHPRESS_CHECKLISTS_LOADED')) {
-    define('PUBLISHPRESS_CHECKLISTS_ITEM_ID', '6465');
     define('PUBLISHPRESS_CHECKLISTS_PATH_BASE', plugin_dir_path(__FILE__));
-    define('PUBLISHPRESS_CHECKLISTS_VERSION', '2.0.0-alpha.1');
+    define('PUBLISHPRESS_CHECKLISTS_VERSION', '2.0.0');
     define('PUBLISHPRESS_CHECKLISTS_FILE', 'publishpress-checklists/publishpress-checklists.php');
-    define('PUBLISHPRESS_CHECKLISTS_MODULES_PATH', __DIR__ . '/modules');
-    define('PUBLISHPRESS_CHECKLISTS_MODULE_PATH', __DIR__ . '/modules/checklists');
-    define('PUBLISHPRESS_CHECKLISTS_ITEM_NAME', 'Checklists');
-    define('PUBLISHPRESS_CHECKLISTS_LIB_PATH', PUBLISHPRESS_CHECKLISTS_PATH_BASE . '/library');
+    define('PUBLISHPRESS_CHECKLISTS_MODULES_PATH', PUBLISHPRESS_CHECKLISTS_PATH_BASE . '/modules');
     define('PUBLISHPRESS_CHECKLISTS_RELATIVE_PATH', 'publishpress-checklists');
     define('PUBLISHPRESS_CHECKLISTS_LOADED', 1);
 
-    if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-        require_once __DIR__ . '/vendor/autoload.php';
+    if (file_exists(PUBLISHPRESS_CHECKLISTS_PATH_BASE . '/vendor/auto   load.php')) {
+        require_once PUBLISHPRESS_CHECKLISTS_PATH_BASE . '/vendor/autoload.php';
     }
 }
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Author URI: https://publishpress.com
 Tags: publishpress, checklist
 Requires at least: 4.6
 Requires PHP: 5.4
-Tested up to: 5.1
+Tested up to: 5.3
 Stable tag: 2.0.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
