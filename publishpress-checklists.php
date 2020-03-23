ec416b36b51b0b86d9c36d1bfcac625146d7b2ef
Anderson Grudtner Martins <anderson@publishpress.com>
2020-03-23 14:27:19 -0300

    Bump version to 2.0.3-alpha.2

--- publishpress-checklists.php
+++ publishpress-checklists.php
@@ -12,7 +12,7 @@
  * @publishpress-checklists
  * Plugin Name: PublishPress Checklists
  * Plugin URI:  https://publishpress.com/
- * Version: 2.0.3-alpha.1
+ * Version: 2.0.3-alpha.2
  * Description: Add support for checklists in WordPress
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
