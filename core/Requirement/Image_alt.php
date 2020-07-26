a0d146bdf36ab503c373c3ec979508b7a81dace6
Olawale Adesina <olawale.olatech.pro@gmail.com>
2020-07-27 00:42:28 +0100

    #149-Force alt tags for all images

---
+++ core/Requirement/Image_alt.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package     PublishPress\Checklists
+ * @author      PublishPress <help@publishpress.com>
+ * @copyright   copyright (C) 2019 PublishPress. All rights reserved.
+ * @license     GPLv2 or later
+ * @since       1.0.0
+ */
+
+namespace PublishPress\Checklists\Core\Requirement;
+
+defined( 'ABSPATH' ) or die( 'No direct script access allowed.' );
+
+class Image_Alt extends Base_simple {
+
+	/**
+	 * The name of the requirement, in a slug format
+	 *
+	 * @var string
+	 */
+	public $name = 'image_alt';
+
+	/**
+	 * Initialize the language strings for the instance
+	 *
+	 * @return void
+	 */
+	public function init_language() {
+		$this->lang['label']          = __( 'Alt text for all images', 'publishpress-checklists' );
+		$this->lang['label_settings'] = __( 'Alt text for all images', 'publishpress-checklists' );
+	}
+
+	/**
+	 * Check for images without alt text from content and return result as array
+	 *
+	 * @param string $content
+	 * @param array $missing_alt
+	 *
+	 * @return array
+	 * @since  1.0.1
+	 */
+	public function missing_alt_images(
+		$content,
+		$image_alt = array()
+	) {
+
+
+		$doc = new DOMDocument();
+		$doc->loadHTML( $content );
+		$images = $doc->getElementsByTagName( 'img' );
+
+		foreach ( $images as $image ) {
+			if ( empty( trim( $image->getAttribute( 'alt' ) ) ) ) {
+				$image_alt[] = $image->getAttribute( 'src' );
+			}
+		}
+
+		// return image alt as array
+		return $image_alt;
+	}
+
+	/**
+	 * Returns the current status of the requirement.
+	 *
+	 * @param stdClass $post
+	 * @param mixed $option_value
+	 *
+	 * @return mixed
+	 */
+	public function get_current_status( $post, $option_value ) {
+		$count = count( $this->missing_alt_images( $post->post_content ) );
+
+		return ( $count == 0 );
+	}
+}
