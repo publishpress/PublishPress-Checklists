cb38c2bf72d29a97b0440e636b9726eb6692f4e0
Olawale Adesina <olawale.olatech.pro@gmail.com>
2020-08-24 02:04:08 +0100

    update class name

--- core/Requirement/Validate_links.php
+++ core/Requirement/Validate_links.php
@@ -11,7 +11,7 @@ namespace PublishPress\Checklists\Core\Requirement;
 
 defined('ABSPATH') or die('No direct script access allowed.');
 
-class Validate_Links extends Base_simple
+class Validate_links extends Base_simple
 {
 
     /**
