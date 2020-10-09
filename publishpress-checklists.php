90808d61c3417be5d514e6a84165cfcad9a831f4
Anderson Grudtner Martins <anderson@publishpress.com>
2020-10-09 11:41:17 -0300

    Update version number

--- publishpress-checklists.php
+++ publishpress-checklists.php
@@ -12,7 +12,7 @@
  * @publishpress-checklists
  * Plugin Name: PublishPress Checklists
  * Plugin URI:  https://publishpress.com/
- * Version: 2.4.1
+ * Version: 2.4.1-hotfix-231-doaction-undefined
  * Description: Add support for checklists in WordPress
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
