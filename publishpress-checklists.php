336157769d0ec9390c7eb8c31d67df6441cba6ba
Anderson Grudtner Martins <anderson@publishpress.com>
2021-03-30 19:20:17 -0300

    Update version number tp v2.4.4-rc.1

--- publishpress-checklists.php
+++ publishpress-checklists.php
@@ -12,7 +12,7 @@
  * @publishpress-checklists
  * Plugin Name: PublishPress Checklists
  * Plugin URI:  https://publishpress.com/
- * Version: 2.4.3
+ * Version: 2.4.4-rc.1
  * Description: Add support for checklists in WordPress
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
