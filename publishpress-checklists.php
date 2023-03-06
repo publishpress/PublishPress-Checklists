31c150427f0ff9b4d75bdc8c06786b37113b6001
Olawale Adesina <olawale.olatech.pro@gmail.com>
2023-03-06 14:23:01 +0100

    - Update the version number to the next stable version.

--- publishpress-checklists.php
+++ publishpress-checklists.php
@@ -12,7 +12,7 @@
  * @publishpress-checklists
  * Plugin Name: PublishPress Checklists
  * Plugin URI:  https://publishpress.com/
- * Version: 2.7.3
+ * Version: 2.7.4
  * Description: Add support for checklists in WordPress
  * Author:      PublishPress
  * Author URI:  https://publishpress.com
