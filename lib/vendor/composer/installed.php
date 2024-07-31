55bd1d277d12d4076d48d9aed6e4e0cff30e2d33
Richa Ferry Setyawan <49858604+richaferry@users.noreply.github.com>
2024-08-01 04:18:06 +0700

    composer update (#725)

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'b0af251792fead193c835cd4cd6cf544ae7f5bfe',
+        'reference' => '37e5020d6fac50b9cacfca0fb6ce1cdf67ff12ed',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'b0af251792fead193c835cd4cd6cf544ae7f5bfe',
+            'reference' => '37e5020d6fac50b9cacfca0fb6ce1cdf67ff12ed',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
